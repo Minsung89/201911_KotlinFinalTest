22b0aa9ac25bbd084fd719d5edd61895caae54bd
Minsung89 <minsung0217@gmail.com>
2019-11-25 20:20:20 +0900

    랜덤넘버 6개를 1~45, 중복 검사를 해서 집어 넣고 출력

--- app/src/main/java/com/tjoeum/a201911_kotlinfinaltest/LottoActivity.kt
+++ app/src/main/java/com/tjoeum/a201911_kotlinfinaltest/LottoActivity.kt
@@ -1,18 +1,63 @@
 package com.tjoeum.a201911_kotlinfinaltest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_lotto.*
 
 class LottoActivity : BaseActivity() {
+
+    var thisWeekLottoNumArrayList = ArrayList<Int>()
+    var thisWeekLottoNumTextViewArrayList = ArrayList<TextView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lotto)
+
+        setupEvent()
+        setValues()
+
     }
 
     override fun setupEvent() {
+
+        buyOneLottoBtn.setOnClickListener {
+            setThisWeekLottoNum()
+        }
+
     }
 
     override fun setValues() {
+        thisWeekLottoNumTextViewArrayList.add(lottoNumTxt1)
+        thisWeekLottoNumTextViewArrayList.add(lottoNumTxt2)
+        thisWeekLottoNumTextViewArrayList.add(lottoNumTxt3)
+        thisWeekLottoNumTextViewArrayList.add(lottoNumTxt4)
+        thisWeekLottoNumTextViewArrayList.add(lottoNumTxt5)
+        thisWeekLottoNumTextViewArrayList.add(lottoNumTxt6)
     }
+
+
+    fun setThisWeekLottoNum(){
+
+
+        for (lottoNumTxt in thisWeekLottoNumTextViewArrayList){
+            var isNumber = true
+            var randomNum = 0
+            while (isNumber){
+                randomNum = (Math.random() * 45 +1).toInt()
+                for (num in thisWeekLottoNumArrayList){
+                    if(randomNum == num){
+                        break
+                    }
+                    isNumber = false
+                }
+            }
+            thisWeekLottoNumArrayList.add(randomNum)
+            lottoNumTxt.text = randomNum.toString()
+        }
+
+
+
+    }
+
+
 }
--- app/src/main/java/com/tjoeum/a201911_kotlinfinaltest/MainActivity.kt
+++ app/src/main/java/com/tjoeum/a201911_kotlinfinaltest/MainActivity.kt
@@ -3,9 +3,12 @@ package com.tjoeum.a201911_kotlinfinaltest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+    var lastBackBtuttonPressedTime = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +16,21 @@ class MainActivity : BaseActivity() {
 
         setupEvent()
         setValues()
+    }
+
+    override fun onBackPressed() {
+//        super.onBackPressed()
+
+        var currentTime = System.currentTimeMillis()
+
+        if(currentTime - lastBackBtuttonPressedTime > 1500){
+            Toast.makeText(mContext, "한번 더 뒤로 버튼을 누르면 앱이 종료됩니다.",Toast.LENGTH_SHORT).show()
+        }else{
+            finish()
+        }
+
+        lastBackBtuttonPressedTime = currentTime
+
     }
 
     override fun setupEvent() {
