6c635c2d44bba24c6f5341e9adbe0bcde86e98df
Minsung89 <minsung0217@gmail.com>
2019-11-25 20:54:51 +0900

    내 마킹 숫자들을 ArrayList에 담아두기

--- app/src/main/java/com/tjoeum/a201911_kotlinfinaltest/LottoActivity.kt
+++ app/src/main/java/com/tjoeum/a201911_kotlinfinaltest/LottoActivity.kt
@@ -3,11 +3,16 @@ package com.tjoeum.a201911_kotlinfinaltest
 import android.os.Bundle
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_lotto.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class LottoActivity : BaseActivity() {
 
-    var thisWeekLottoNumArrayList = ArrayList<Int>()
+    var lottoNumArrayList = ArrayList<Int>()
     var thisWeekLottoNumTextViewArrayList = ArrayList<TextView>()
+
+    var myNumArrayList = ArrayList<Int>()
+    var myNumTextViewArrayList = ArrayList<TextView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,29 +38,85 @@ class LottoActivity : BaseActivity() {
         thisWeekLottoNumTextViewArrayList.add(lottoNumTxt4)
         thisWeekLottoNumTextViewArrayList.add(lottoNumTxt5)
         thisWeekLottoNumTextViewArrayList.add(lottoNumTxt6)
+
+        myNumTextViewArrayList.add(myLottoNumTxt1)
+        myNumTextViewArrayList.add(myLottoNumTxt2)
+        myNumTextViewArrayList.add(myLottoNumTxt3)
+        myNumTextViewArrayList.add(myLottoNumTxt4)
+        myNumTextViewArrayList.add(myLottoNumTxt5)
+        myNumTextViewArrayList.add(myLottoNumTxt6)
+
+        for (myTv in myNumTextViewArrayList){
+            myNumArrayList.add(myTv.text.toString().toInt())
+        }
+
+    }
+
+    fun checkLottoRank(){
+
+        //6 : 1등 => 20억원
+        //5 : 3등 => 150만원
+        //4 : 4등 => 5만원
+        //3 : 5등 => 5천원
+        //그 이하 => 0원
+
+
     }
 
 
-    fun setThisWeekLottoNum(){
+    fun setThisWeekLottoNum() {
 
+//        당첨번호는 모두 날리고 다시 뽑자
+        lottoNumArrayList.clear()
 
-        for (lottoNumTxt in thisWeekLottoNumTextViewArrayList){
-            var isNumber = true
+//        6개의 텍스트뷰에 들어갈 당첨번호를 뽑아내는 반복문
+        for (lottoNumTxt in thisWeekLottoNumTextViewArrayList) {
+
+//            선정된 랜덤값이 들어갈 변수
             var randomNum = 0
-            while (isNumber){
-                randomNum = (Math.random() * 45 +1).toInt()
-                for (num in thisWeekLottoNumArrayList){
-                    if(randomNum == num){
+
+//            몇번을 반복해야 중복을 피할지 알 수 없다.
+//            반복문 : 횟수가 명확하면 for. 언제까지 돌려야할지 모르면 while(true) => if(조건) break
+            while (true) {
+
+//                일단 중복되지 않는다.(괜찮다)라고 전제하고 검사 시작
+                var isDuplOk = true
+
+//                1~45 사이의 랜덤값을 뽑아서 변수에 임시 저장
+                randomNum = (Math.random() * 45 + 1).toInt()
+
+//                지금까지 만든 당첨번호를 모두 꺼내어보자.
+                for (num in lottoNumArrayList){
+
+//                    지금 만든 랜덤번호와 꺼내본 당첨번호가 같은가?
+                    if(num == randomNum) {
+//                        중복되는 숫자를 발견!
+//                        더이상 중복검사를 통과할 수 없다.
+                        isDuplOk = false
                         break
                     }
-                    isNumber = false
+                }
+//              중복검사를 통과했는지 확인.
+                if(isDuplOk) {
+//                    만약에 통과했다면 당첨번호로 넣어주자.
+                    lottoNumArrayList.add(randomNum)
+//                    올바른 번호를 뽑았으니 무한반복을 탈출
+                    break
                 }
             }
-            thisWeekLottoNumArrayList.add(randomNum)
-            lottoNumTxt.text = randomNum.toString()
+//              순서가 제멋대로여서 보기 안좋다.
+//            lottoNumTxt.text = randomNum.toString()
         }
+//      당첨번호 6개를 작은 숫자부터 큰 숫자 순서대로(정렬)!
+        Collections.sort(lottoNumArrayList)
 
+//        6개의 텍스트뷰 / 숫자를 뽑아내서 연결.
+        for (i in 0..lottoNumArrayList.size-1) {
+            var numTxt = thisWeekLottoNumTextViewArrayList.get(i)
+            var number = lottoNumArrayList.get(i)
 
+            numTxt.text = number.toString()
+        }
 
     }
 
