a6a03e6b27e66b6ba53efda87e5bbef182920e14
Minsung89 <minsung0217@gmail.com>
2019-11-27 20:07:39 +0900

    두개의 프레그먼트를 ViepPager 표시

--- app/src/main/java/com/tjoeum/a201911_kotlinfinaltest/BoardActivity.kt
+++ app/src/main/java/com/tjoeum/a201911_kotlinfinaltest/BoardActivity.kt
@@ -3,8 +3,10 @@ package com.tjoeum.a201911_kotlinfinaltest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.tjoeum.a201911_kotlinfinaltest.adapters.ViewPagerAdapter
 import com.tjoeum.a201911_kotlinfinaltest.datas.User
 import com.tjoeum.a201911_kotlinfinaltest.utils.GlobalData
+import kotlinx.android.synthetic.main.activity_board.*
 
 class BoardActivity : BaseActivity() {
 
@@ -22,5 +24,9 @@ class BoardActivity : BaseActivity() {
 
     override fun setValues() {
         Log.d("가져온값",GlobalData.loginUserData.toString())
+
+        myVP.adapter = ViewPagerAdapter(supportFragmentManager)
+
+
     }
 }
---
+++ app/src/main/java/com/tjoeum/a201911_kotlinfinaltest/adapters/ViewPagerAdapter.kt
@@ -0,0 +1,26 @@
+package com.tjoeum.a201911_kotlinfinaltest.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.tjoeum.a201911_kotlinfinaltest.fragments.BoardListFragment
+import com.tjoeum.a201911_kotlinfinaltest.fragments.NoticeListFragment
+
+class ViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+
+
+    override fun getItem(position: Int): Fragment {
+
+        return when(position){
+
+            0 -> BoardListFragment()
+            else -> NoticeListFragment()
+
+        }
+
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+}
